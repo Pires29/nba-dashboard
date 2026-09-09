14cffe3ed93e708f14c80ff7fd791a2d9be628fd
Renato Pires <renato.pires@custojusto.pt>
2026-09-09 16:55:17 +0100

    fix: allow prisma default datasource config

--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -3,13 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global;
 
 if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  const prismaOptions = process.env.DATABASE_URL
+    ? {
+        datasources: {
+          db: {
+            url: process.env.DATABASE_URL,
+          },
+        },
+      }
+    : {};
+
+  globalForPrisma.prisma = new PrismaClient(prismaOptions);
 }
 
 const prisma = globalForPrisma.prisma;
