70566a79441f957160d023d84e52efdb1f2423f8
Renato Pires <renato.pires@custojusto.pt>
2026-09-15 15:30:32 +0100

    Revert "test: temporarily hide landing showcase"
    This reverts commit d7667bc1523581fc5f4d79ea7993ceb86a516b11.

--- src/components/home/HomeLanding.js
+++ src/components/home/HomeLanding.js
@@ -3,9 +3,7 @@ import { launchConfig } from "@/config/launch";
 import { ALL_FAQS } from "@/lib/faqs";
 import PricingLink from "@/components/PricingLink";
 import HomePricingSection from "./HomePricingSection";
-// Temporary mobile-rendering diagnostic: re-enable once the below-the-fold
-// rendering test is complete.
-// import DeferredProductShowcase from "./DeferredProductShowcase";
+import DeferredProductShowcase from "./DeferredProductShowcase";
 import PublicAccessButton from "./PublicAccessButton";
 
 const BENEFITS = [
@@ -49,7 +47,7 @@ export default function HomeLanding() {
         </div>
       </section>
 
-      {/* <DeferredProductShowcase /> */}
+      <DeferredProductShowcase />
 
       <section id="features" className="relative scroll-mt-20 border-y border-white/[0.06] bg-[#09111d]/80 py-24">
         <div className="mx-auto max-w-[1320px] px-6">
